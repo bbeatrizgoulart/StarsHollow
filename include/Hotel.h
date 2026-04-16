0a26ac51ba4f74b0aa859317a1051a989e2c3ed3
bbeatrizgoulart <goulartbeatriz@outlook.com>
2026-04-16 12:02:10 -0300

    Evolução: Adicionado sistema de limpeza de tela e lista dinâmica de hóspedes

--- include/Hotel.h
+++ include/Hotel.h
@@ -2,27 +2,46 @@
 #define HOTEL_H
 
 #include "Estabelecimento.h"
+#include <vector>
+#include <string>
 
 class Hotel : public Estabelecimento {
 private:
     int capacidade;
-    int ocupados;
+    std::vector<std::string> listaHospedes;
 
 public:
     Hotel(std::string _n, std::string _r, int _cap)
-    : Estabelecimento(_n, _r), capacidade(_cap), ocupados(0) {}
+    : Estabelecimento(_n, _r), capacidade(_cap) {}
 
     void BoasVindas() override {
         Estabelecimento::BoasVindas();
-        std::cout << "Quartos disponiveis: " << (capacidade - ocupados) << std::endl;
+
+        std::cout << "Capacidade total: " << capacidade << " quartos." << std::endl;
+        std::cout << "Quartos ocupados: " << listaHospedes.size() << std::endl;
+
+        if (!listaHospedes.empty()) {
+            std::cout << "\nHospedes atuais no Independence Inn: " << std::endl;
+            for (const auto& nome : listaHospedes) {
+                std::cout << "- " << nome << std::endl;
+            }
+        } else {
+            std::cout << "O hotel esta vazio no momento. Michel esta entendiado.";
+        }
     }
     
     void fazerCheckIn() {
-        if (ocupados < capacidade) {
-            ocupados++;
-            std::cout << "Check-in realizado! Michel esta levando suas malas." << std::endl;
+        if (listaHospedes.size() < capacidade) {
+            std::string nomeNovo;
+            std::cout << "Digite o nome do hospede para registro: ";
+
+            std::cin.ignore();
+            std::getline(std::cin, nomeNovo);
+
+            listaHospedes.push_back(nomeNovo);
+            std::cout << "Check-in realizado! Bem-vindo(a), " << nomeNovo << "! Michel esta levando suas malas." << std::endl;
         } else {
-            std::cout << "Estamos lotado! Tente a Dragonfly Inn." << std::endl;
+            std::cout << "Lamentamos, mas estamos lotados! Tente a Dragonfly Inn." << std::endl;
         }
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
+#include <cstdlib>
 #include "include/Hotel.h"
 
 using namespace std;
 
 int main() {
-
     Hotel independenceInn("Independence Inn", "Lorelai Gilmore", 10);
-
     int opcao = 0;
 
-    cout << "--- BEM-VINDO A STARS HOLLOW ---" << endl;
-
     while (opcao != 3) {
+        system("clear");
+
+        cout << "--- BEM-VINDO A STARS HOLLOW ---" << endl;
         cout << "O que voce gostaria de fazer?" << endl;
         cout << "1. Visitar a recepcao (Boas-vindas)" << endl;
         cout << "2. Tentar fazer o Check-in" << endl;
@@ -28,15 +28,24 @@ int main() {
         switch (opcao) {
             case 1:
                 independenceInn.BoasVindas();
+                cout << "\nPressione Enter para voltar ao menu...";
+                cin.ignore();
+                cin.get();
                 break;
             case 2:
                 independenceInn.fazerCheckIn();
+                cout << "\nPressione Enter para voltar ao menu...";
+                cin.ignore();
+                cin.get();
                 break;
             case 3:
+                system("clear");
                 cout << "Saindo de Stars Hollow... Pegando a estrada para Hartford." << endl;
                 break;
             default:
                 cout << "Opcao invalida! Taylor Doose esta furioso!" << endl;
+                cin.ignore();
+                cin.get();
         }
     }
 
